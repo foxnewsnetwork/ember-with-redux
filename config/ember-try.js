1a694390d7e5a6e1195a9972dd916e494d9692c8
Thomas Chen <tom.chen@am.sony.com>
2016-09-15 17:59:26 -0700

    updated try file

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,53 +2,84 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      bower: {
-        dependencies: { }
+      name: 'ember-2.0.X',
+      dependencies: {
+        'ember': '~2.0.0'
+      },
+      resolutions: {
+        'ember': '~2.0.0'
       }
     },
     {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
-        }
+      name: 'ember-2.1.X',
+      dependencies: {
+        'ember': '~2.1.0'
+      },
+      resolutions: {
+        'ember': '~2.1.0'
       }
     },
     {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
+      name: 'ember-2.2.X',
+      dependencies: {
+        'ember': '~2.2.0'
+      },
+      resolutions: {
+        'ember': '~2.2.0'
+      }
+    },
+    {
+      name: 'ember-2.3.X',
+      dependencies: {
+        'ember': '~2.3.0'
+      },
+      resolutions: {
+        'ember': '~2.3.0'
+      }
+    },
+    {
+      name: 'ember-2.4.X',
+      dependencies: {
+        'ember': '~2.4.0'
+      },
+      resolutions: {
+        'ember': '~2.4.0'
+      }
+    },
+    {
+      name: 'ember-2.5.X',
+      dependencies: {
+        'ember': '~2.5.0'
+      },
+      resolutions: {
+        'ember': '~2.5.0'
+      }
+    },
+    {
+      name: 'ember-2.6.X',
+      dependencies: {
+        'ember': '~2.6.0'
+      },
+      resolutions: {
+        'ember': '~2.6.0'
+      }
+    },
+    {
+      name: 'ember-2.7.X',
+      dependencies: {
+        'ember': '~2.7.0'
+      },
+      resolutions: {
+        'ember': '~2.7.0'
       }
     },
     {
       name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
       }
     }
   ]
